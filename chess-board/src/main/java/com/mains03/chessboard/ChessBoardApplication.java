e6947b8158b8351911e6bc1b880a9c2166f4eb6e
Samson Main <ib21231@bristol.ac.uk>
2023-02-05 14:42:31 +0000

    initial configuration

---
+++ chess-board/src/main/java/com/mains03/chessboard/ChessBoardApplication.java
@@ -0,0 +1,13 @@
+package com.mains03.chessboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessBoardApplication.class, args);
+	}
+
+}
