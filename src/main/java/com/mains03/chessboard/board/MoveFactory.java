7e42e53e20b69650f3755b38c01f70da00974574
Samson Main <Samson Main>
2023-02-23 16:44:48 +0000

    pass tests

--- src/main/java/com/mains03/chessboard/board/MoveFactory.java
+++ src/main/java/com/mains03/chessboard/board/MoveFactory.java
@@ -1,5 +1,8 @@
 package com.mains03.chessboard.board;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MoveFactory {
     private static MoveFactory instance;
 
@@ -11,9 +14,44 @@ public class MoveFactory {
         return instance;
     }
 
-    private MoveFactory() {}
+    private final Pattern movePattern;
+
+    private MoveFactory() {
+        String pattern = sqaurePattern()
+                + sqaurePattern()
+                + promotedToPattern();
+
+        movePattern = Pattern.compile(pattern);
+    }
+
+    private String sqaurePattern() {
+        String[] fileLetter = new String[] {
+                "a","b","c","d","e","f","g","h"
+        };
+        String[] rankNumber = new String[] {
+                "1","2","3","4","5","6","7","8"
+        };
+
+        String pattern = "[";
+        for (String s : fileLetter)
+            pattern += s;
+        pattern += "][";
+        for (String s : rankNumber)
+            pattern += s;
+        pattern += "]";
+        return pattern;
+    }
+
+    private String promotedToPattern() {
+        return "[qrbn]?";
+    }
 
     public Move createMove(String moveString) {
-        return null;
+        Matcher matcher = movePattern.matcher(moveString);
+        if (matcher.matches()) {
+            return new Move() {};
+        } else {
+            return null;
+        }
     }
 }
